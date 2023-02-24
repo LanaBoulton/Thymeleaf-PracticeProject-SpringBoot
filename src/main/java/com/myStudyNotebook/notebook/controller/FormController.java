4ee9dafecd6dfb0055e363522f4027bade998eab
Lana <gavrylovalova@gmail.com>
2023-02-24 12:02:29 -0800

    submission form added

--- src/main/java/com/myStudyNotebook/notebook/controller/FormController.java
+++ src/main/java/com/myStudyNotebook/notebook/controller/FormController.java
@@ -4,6 +4,8 @@ import com.myStudyNotebook.notebook.model.UserForm;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 
 import java.util.Arrays;
 import java.util.List;
@@ -14,7 +16,7 @@ public class FormController {
     //handler method to dandle user registration request
     //
     @GetMapping("register")
-    public String userRegistrationPage(Model model){
+    public String userRegistrationPage(Model model) {
         // Emptu user form model object to store form data
         UserForm userForm = new UserForm();
         model.addAttribute("userForm", userForm);
@@ -22,6 +24,13 @@ public class FormController {
         List<String> listProfession = Arrays.asList("Developer", "tester", "Architect");
         model.addAttribute("listProfession", listProfession);
         return "register-form"; //match html file name
+    }
 
+    //handler method to handle user registration form submission request
+
+    @PostMapping("register/save")
+    public String submitForm(Model model, @ModelAttribute("userForm") UserForm userForm) {
+        model.addAttribute("userForm", userForm);
+        return "register-success";
     }
 }
